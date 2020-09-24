dcf5edc0c56d7c7afd2ac38fb973d5b9e512e66e
Caleb <caleb@dedicatedmc.io>
2020-09-24 12:06:21 -0400

    It works!

--- CareGiver.java
+++ CareGiver.java
@@ -1,19 +1,34 @@
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+
 class CareGiver extends Thread 
 { 
+    Semaphore sem; 
+    String name; 
+    Random r = new Random();
+
+    public CareGiver(Semaphore sem, String name) {
+        this.sem = sem;
+        this.name = name;
+    }
+    
     public void run() 
     { 
         try
-        { 
-            // Displaying the thread that is running 
-            System.out.println ("Caregiver " + 
-                  Thread.currentThread().getId() + 
-                  " is running"); 
-  
+        {
+            System.out.println(this.name + " moved into the apartment!");
+            while(true) {
+                Thread.sleep(r.nextInt(9000-3000) + 3000);
+                if(sem.tryAcquire()) {
+                    System.out.println(this.name + " feed the fish!");
+                } else {
+                    System.out.println(this.name + " tried to feed the fish but he was already fed.");
+                }
+            }
         } 
         catch (Exception e) 
-        { 
-            // Throwing an exception 
-            System.out.println ("Exception is caught"); 
+        {  
+            System.out.println ("An error has occurred: " + e.toString()); 
         } 
     } 
 } 
--- HouseHold.java
+++ HouseHold.java
@@ -1,13 +1,27 @@
+import java.util.concurrent.*;
+import java.util.TimerTask;
+import java.util.Timer;
+
 // Main Class 
 public class HouseHold 
 { 
     public static void main(String[] args) 
     { 
-        int n = 8; // Number of threads 
-        for (int i=0; i<n; i++) 
-        { 
-            CareGiver object = new CareGiver(); 
-            object.start(); 
-        } 
+        Semaphore sem = new Semaphore(1);
+
+        CareGiver me = new CareGiver(sem, "Caleb");
+        CareGiver so = new CareGiver(sem, "Sarah");
+        me.start();
+        so.start();
+
+        Timer t = new Timer( );
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                System.out.println("It's a new day!");
+                sem.release();
+            }
+        }, 0, 10000);
     } 
 } 
