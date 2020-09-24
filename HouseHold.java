b422746cfb85e57285ba35acc03718a0bc4c75b0
Caleb <caleb@dedicatedmc.io>
2020-09-24 12:17:30 -0400

    Fixed bug causing fish to get fed twice

--- HouseHold.java
+++ HouseHold.java
@@ -20,7 +20,13 @@ public class HouseHold
             @Override
             public void run() {
                 System.out.println("It's a new day!");
-                sem.release();
+                // This shouldn't happen except possibily in the very beginning while the caregivers are moving in. AKA when the threads are first starting.
+                // If a new day happens at the beginning before the threads finish initializing it could happen. In this case the fish would get fed twice
+                // because there would be 2 available permits. I could count this as the fish dying but this will never happen except for the case stated
+                // above.
+                if(sem.availablePermits() < 1) {
+                    sem.release();
+                }
             }
         }, 0, 10000);
     } 
