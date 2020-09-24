7940d14370fee79c1857dc6fc76ce108963f15d8
Caleb <caleb@dedicatedmc.io>
2020-09-24 11:31:26 -0400

    Multithreading working

--- HouseHold.java
+++ HouseHold.java
@@ -1,12 +1,12 @@
 // Main Class 
-public class Multithread 
+public class HouseHold 
 { 
     public static void main(String[] args) 
     { 
         int n = 8; // Number of threads 
         for (int i=0; i<n; i++) 
         { 
-            MultithreadingDemo object = new MultithreadingDemo(); 
+            CareGiver object = new CareGiver(); 
             object.start(); 
         } 
     } 
